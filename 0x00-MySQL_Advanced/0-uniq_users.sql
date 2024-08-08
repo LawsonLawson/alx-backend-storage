d87c8c58419a6aa2410d6b83c55a7968d4579cd7
Lawson Israel Pascal <isreallawson@outlook.com>
2024-08-08 14:24:55 +0000

    a SQL script that creates a table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creates a `users` table if it does not already exist.
 -- The table includes the following columns:
---   - `id`: An integer that serves as the primary key, automatically incremented for each new record. 
+--   - `id`: An integer that serves as the primary key, automatically incremented for each new record.
 --   - `email`: A VARCHAR field that stores the user's email address, which must be unique and cannot be null.
 --   - `name`: A VARCHAR field that stores the user's name, which is optional (can be null).
 
